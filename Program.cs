6d7131bb2f3964791e77c86283ff6fd23cc90ebd
Will Harris <Harriswill22@yahoo.com>
2019-05-13 10:47:36 -0400

    setting up C# project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace democonsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Will";
+            Console.WriteLine(name);
+        }
+    }
+}
