3be61f2a8547a137d83adae6f9e25091fce8a82e
Will Harris <Harriswill22@yahoo.com>
2019-05-13 11:01:51 -0400

    number Guesser app

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 
-namespace democonsole
+namespace NumberGuesser
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string name = "Will";
-            Console.WriteLine(name);
+            //Set app vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Will Harris";
+
+            Console.WriteLine("{0}: Version {1} by {2}", appName,appVersion, appAuthor);
         }
     }
 }
